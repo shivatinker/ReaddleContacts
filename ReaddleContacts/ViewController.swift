9d7ea4d6ee9e00cbac1a462193f5c4d0941cf928
Andrii Zinoviev <zinoviev@stud.onu.edu.ua>
2020-05-25 16:49:53 +0300

    Initial Commit

---
+++ ReaddleContacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReaddleContacts
+//
+//  Created by Andrii Zinoviev on 25.05.2020.
+//  Copyright © 2020 Andrii Zinoviev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
