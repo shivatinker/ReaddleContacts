7b39e78b5442a3efb443c63458732414de12644c
Andrii Zinoviev <zinoviev@stud.onu.edu.ua>
2020-05-30 20:54:52 +0300

    Fix

--- ReaddleContacts/DataProviders/DataProviderCommon.swift
+++ ReaddleContacts/DataProviders/DataProviderCommon.swift
@@ -13,24 +13,6 @@ public protocol ErrorHandler {
     func error(_ e: Error)
 }
 
-public enum ConditionalResult<T, E: Error> {
-    case success(result: T)
-    case failure(error: E)
-
-    /// Unwraps result
-    /// If result is success returns unwrapped result object
-    /// If result is failure passes an error to errorHandler and returns nil
-    /// - Parameter errorHandler: ErrorHandler to handle failure case
-    /// - Returns: Optional T value
-    public func unwrap(errorHandler: ErrorHandler? = nil) -> T? {
-        switch self {
-        case .failure(let error): errorHandler?.error(error)
-        case .success(let result): return result
-        }
-        return nil
-    }
-}
-
 public struct DataContext {
     public let contact: ContactsProvider
     public let gravatar: GravatarAPI
@@ -40,27 +22,57 @@ public struct DataContext {
         let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         return String((0..<length).map { _ in letters.randomElement()! })
     }
-
+    
+    /// Gets contact info and online status
+    /// - Parameter id: contact id
+    /// - Returns: Promise
+    public func getContactInfoAndOnline(for id: ContactID) -> Promise<(Contact, Bool)> {
+        return when(fulfilled: self.contact.getContact(id: id),
+                    self.contact.isOnline(id: id))
+    }
+    
+    /// Simulates changes, adds and removes `amount` new random contacts, updates online statuses
+    /// - Parameter amount: number of contacts to be added and removed
+    /// - Returns: Promise
     public func simulateChanges(amount: Int = 30) -> Promise<Void> {
         firstly {
-            getAllContactP()
+            self.contact.getAllContacts()
         }.map { contacts in
             return Array(contacts.map({ (id, _) in id }).shuffled().prefix(amount))
         }.then { ids in
             when(fulfilled: ids.map { id in
-                self.removeContactP(id: id)
+                self.contact.removeContact(id: id)
             }).asVoid()
         }.then { _ in
             self.addRandomContacts(count: amount)
         }.then {
-            Promise { seal in
-                self.contact.updateOnline { (res) in
-                    self.applyResult(res, seal)
+            self.contact.updateOnline()
+        }
+    }
+    
+    /// Loads avatar for specified contact `id`
+    /// - Parameters:
+    ///   - id: Contact id
+    ///   - size: Requested avatar size
+    /// - Returns: Promise
+    public func getAvatar(for id: ContactID, size: Int) -> Promise<UIImage?> {
+        let params = GravatarParams(taskId: id, size: size, defaultAvatar: .identicon)
+        return firstly {
+            self.contact.getContact(id: id)
+        }.then { (contact: Contact) -> Promise<UIImage?> in
+            if let email = contact.email {
+                return self.gravatar.getAvatarImage(email: email, params: params)
+            } else {
+                return Promise<UIImage?> { seal in
+                    seal.fulfill(nil)
                 }
             }
         }
     }
-
+    
+    /// Adds `count` random contacts
+    /// - Parameter count: Number of contacts to be added
+    /// - Returns: Promise
     public func addRandomContacts(count: Int) -> Promise<Void> {
         randomInfo.getRandomNames(count: count).map { infos in
             infos.map({ Contact(firstName: $0.firstName,
@@ -68,7 +80,7 @@ public struct DataContext {
                                 email: $0.email) })
         }.then { contacts in
             when(fulfilled: contacts.map { contact in
-                self.addContactP(contact).asVoid()
+                self.contact.addContact(contact).asVoid()
             })
         }
     }
